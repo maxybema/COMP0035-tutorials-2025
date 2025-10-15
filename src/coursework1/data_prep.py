96beab6efe8f65117c7b225a287e1d34e424d332
Max Ybema <maxybema@gmail.com>
2025-10-15 22:02:29 +0100

    Data file 1

--- src/coursework1/data_prep.py
+++ src/coursework1/data_prep.py
@@ -1,1 +1,16 @@
-"""Comment test"""
+import pandas as pd
+from pathlib import Path
+
+# Locates data folder relative to current file
+data = Path(__file__).parent.parent.parent.joinpath('Data')
+
+#Collects all the .csv file paths in the data folder 
+file_paths = sorted(data.glob("*.csv"), key = lambda f: int(f.stem[-4:]))
+
+#Combines all the data files into one dataframe
+flat_prices_df = pd.concat(
+    [pd.read_csv(file_path).assign(source=file_path.stem) for file_path in file_paths],
+    ignore_index = True
+)
+pd.set_option("display.max_columns", None)
+
