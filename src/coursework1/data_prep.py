f92de0b3113ca67a8bce152f7014cf2300777c46
Max Ybema <maxybema@gmail.com>
2025-10-18 03:35:34 +0100

    Updated file saving and testing folder

--- src/coursework1/data_prep.py
+++ src/coursework1/data_prep.py
@@ -4,6 +4,7 @@ from pathlib import Path
 # Locates data folder relative to current file
 data = Path(__file__).parent.parent.parent.joinpath('Data')
 
+
 #Collects all the .csv file paths in the data folder 
 file_paths = sorted(data.glob("*.csv"), key = lambda f: int(f.stem[-4:]))
 
@@ -13,3 +14,18 @@ flat_prices_df = pd.concat(
     ignore_index = True
 )
 pd.set_option("display.max_columns", None)
+
+#Outputs df to Tests folder without index numbers
+tests = Path(__file__).parent.parent.parent.joinpath('Tests')
+flat_prices_df.to_csv("Tests/Flat_Prices_Df.csv", index = False)
+
+def overview():
+    """Describes the basic structure and size of the df as well as the columns and their data types, also shows the first 5 rows as an example
+    
+           Parameters:
+           df (pandas.DataFrame): The dataframe to describe
+    
+           Returns:
+           None
+    
+        """
